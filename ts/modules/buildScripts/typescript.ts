d04918aabb786dd537cbef6119dbc6b2b29db20c
0J3 <0J3@wafflepowered.com>
2021-03-24 15:51:29 +0100

    e

--- ts/modules/buildScripts/typescript.ts
+++ ts/modules/buildScripts/typescript.ts
@@ -1,9 +1,15 @@
 import ts from 'typescript';
 
-const source = "let x: string  = 'string'";
+const source = `let x: string  = 'string'`;
 
 let result = ts.transpileModule(source, {
-	compilerOptions: { module: ts.ModuleKind.CommonJS },
+	compilerOptions: {
+		module: ts.ModuleKind.CommonJS,
+		target: ts.ScriptTarget.Latest,
+		sourceMap: true,
+		removeComments: true,
+		inlineSourceMap: true,
+	},
 });
 
 console.log(JSON.stringify(result));
